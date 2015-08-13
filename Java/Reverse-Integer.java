246573d036c33934947e3a00ee980f3da52f4b04
Lawrence <jiangyi0425@gmail.com>
2015-08-13 23:28:25 +0800

    Cpp

--- Java/Reverse-Integer.java
+++ Java/Reverse-Integer.java
@@ -1,5 +1,11 @@
 public class Solution {
-    public int reverse(int x) {
-        
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int num = x, cur = 0;
+        while (x > 0) {
+            cur = cur * 10 + x % 10;
+            x = x / 10;
+        }
+        return num == cur;
     }
 }
