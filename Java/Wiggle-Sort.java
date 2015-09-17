c2dd97781d5f4b05c6207ef89e8d9b529d626070
Lawrence <jiangyi0425@gmail.com>
2015-09-17 13:56:48 +0800

    Java

---
+++ Java/Wiggle-Sort.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void wiggleSort(int[] nums) {
+        for (int i = 1; i < nums.length; i ++) {
+            if ((i & 1) == 0) {
+                if (nums[i] > nums[i - 1]) {
+                    swap(nums, i, i - 1);
+                }
+            } else {
+                if (nums[i] < nums[i - 1]) {
+                    swap(nums, i, i - 1);
+                }
+            }
+        }
+    }
+}
